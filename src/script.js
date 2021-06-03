b44248007810e545f95bfd39d02cf9bfc150b9b2
Egor Aryamov <eariamov.exceedteam@gmail.com>
2021-06-03 19:27:24 +0300

    added render function

--- src/script.js
+++ src/script.js
@@ -1,27 +1,38 @@
 let consumpArr = [];
+let scoreNameInput;
+let howMuchInput;
+let addConsumption;
 
 window.onload = async () => {
     const response = await fetch ('http://localhost:8000/allConsumptions', {
         method: 'GET',
     })
     const result = await response.json();
-    consumpArr = result.data || []
+    consumpArr = result.data || [];
+    scoreNameInput = document.getElementById('consumption-name');
+    howMuchInput = document.getElementById('consumption-value');
+    addConsumption = document.getElementsByClassName('add')[0];
+
+    addConsumption.addEventListener('click', () => createNewConsumption());
 }
+console.log('ahahah');
 
-const scoreNameInput = document.querySelector('#consumption-name');
-const howMuchInput = document.querySelector('#consumption-value');
-const addConsumption = document.querySelector('.add');
 
+console.log('lol', addConsumption);
 // current values score's and cost's in input
-let scoreNameInputCurrentValue = scoreNameInput.addEventListener('change', (e) => currentValue);
-let howMuchInputCurrentValue = howMuchInput.addEventListener('change', (e) => currentValue);
+let scoreNameInputCurrentValue = '';
+let howMuchInputCurrentValue = '';
+howMuchInput.addEventListener('change', (e) => currentValue);
+scoreNameInput.addEventListener('change', (e) => currentValue);
+
 
 let currentValue = (event) => event.target.value;
 
-addConsumption.onclick = () => createNewConsumption;
+
 
 // add new consumption function
 const createNewConsumption = async () => {
+    console.log('fuck')
     if (isEmpty(scoreNameInputCurrentValue) && isEmpty(howMuchInputCurrentValue)) {
         const response = await fetch('http://localhost:8000/createConsumption', {
             method: 'POST',
@@ -29,13 +40,17 @@ const createNewConsumption = async () => {
                 'Access-Control-Allow-Origin': '*',
                 'Content-Type': 'application/json;charset=utf-8'
             },
-            body: {
+            body: JSON.stringify ({
                 score: isEmpty(scoreNameInputCurrentValue),
                 cost: isEmpty(howMuchInputCurrentValue)
-            }
+            })
         })
         const result = await response.json();
-        consumpArr = resul.data;
+        consumpArr = result.data;
+        scoreNameInput.value = '';
+        howMuchInput.value = '';
+        scoreNameInputCurrentValue = '';
+        howMuchInputCurrentValue = '';
         renderCostList();
     }
 }
@@ -46,3 +61,69 @@ const isEmpty = (string) => {
     string = string.trim()
     return string ? string : false;
 }
+
+// render all Notes
+const renserCostList = () => {
+    const list = document.querySelector('.consumptions-list');
+
+    while(list.firstChild){
+        list.removeChild(list.firstChild);
+    }
+
+    consumpArr.map((elem, index) => {
+        const {pencil, garbage} = images(elem, index);
+        const li = document.createElement('li');
+        li.innerText = `${index + 1}) `;
+        li.append(scoreName(elem, index), date(elem, imdex), sum(elem,index), pencil, garbage);
+        list.append(li);
+    })
+}
+
+const scoreName = (elem, index) => {
+    const p = document.createElement('p');
+    p.value = elem.score;
+    p.id = `score-${elem._id}`;
+    p.className = 'name';
+    p.ondblclick = (index) => editScoreName();
+
+    return p;
+}
+
+const date = (elem, index) => {
+    const p = document.createElement('p');
+    p.value = elem.date;
+    p.className = 'date';
+    p.id = `date-${elem._id}`
+    p.ondblclick = (index) => editDate();
+
+    return p;
+}
+
+const sum = (elem, index) => {
+    const p = document.createElement('p');
+    p.value = elem.cost;
+    p.className = `cost`;
+    p.id = `cost-${elem.id}`
+    p.ondblclick = (index) => editCost();
+
+    return p;
+}
+
+const images = (elem, index) => {
+    const pencil = document.createElement('img');
+    const garbage = document.createElement('img');
+
+    pencil.src = 'img/edit';
+    pencil.className = 'edit';
+    pencil.id = `edit-${elem.id}`;
+    pencil.onclick = () => editThisNote();
+
+    garbage.src = 'img/delete';
+    garbage.className = 'delete';
+    garbage.id = `delete-${elem.id}`;
+    garbage.onclick = () => deleteThisNote();
+
+    return {pencil, garbage};
+}
+
+
